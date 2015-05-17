a6e2ccd5e448940cb66c00a101d4b1b0554b82d7
Volodymyr Miz <mizvladimir@gmail.com>
2015-05-17 04:06:42 -0700

    EX6 - completed assignment

--- ex6/ex6/dataset3Params.m
+++ ex6/ex6/dataset3Params.m
@@ -23,9 +23,30 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+steps = [0.01 0.03 0.1 0.3 1 3 10 30];
 
+minError = Inf;
+minC = Inf;
+minSigma = Inf;
 
+for i = 1:length(steps)
+    for j = 1:length(steps)
+        currentC = steps(i);
+        currentSigma = steps(j);
+        model = svmTrain(X, y, currentC, @(x1, x2) gaussianKernel(x1, x2, currentSigma));
+        predictions = svmPredict(model, Xval);
+        error = mean(double(predictions ~= yval));
 
+        if error < minError
+            minError = error;
+            minC = currentC;
+            minSigma = currentSigma;
+        end
+    end
+end
+
+C = minC;
+sigma = minSigma;
 
 
 
